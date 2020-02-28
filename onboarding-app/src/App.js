007ed50bfcaedad87478d6fb827d68ab76a93c34
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-02-27 21:49:16 -0600

    completed mvp

--- onboarding-app/src/App.js
+++ onboarding-app/src/App.js
@@ -1,44 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import './App.css';
 
+const flexColumn= {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '50%',
+  alignItems: 'center'
+}
+
+const errorStyle = {
+  fontSize: '0.5rem',
+  color: 'red'
+}
+
+
+
 const App = (props) => {
-  const flexColumn= {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '50%',
-    alignItems: 'center'
-  }
 
-  const errorStyle = {
-    fontSize: '0.5rem',
-    color: 'red'
-  }
+  const [users, setUsers] = useState([])
 
-  console.log(props)
+  console.log(props.status)
+  
+  useEffect(() => {
+    props.status && setUsers([...users, props.values])
+  }, [props.status])
 
   return (
-    <Form style={flexColumn}>
-      <label htmlFor="name">Name:</label>
-      <Field type="text" name="name"/>
-      {props.errors.name && props.touched.name && (<p style={errorStyle}>{props.errors.name}</p>)}
+    <div>
+      <Form style={flexColumn}>
+        <label htmlFor="name">Name:</label>
+        <Field type="text" name="name"/>
+        {props.errors.name && props.touched.name && (<p style={errorStyle}>{props.errors.name}</p>)}
 
-      <label htmlFor="email">Email:</label>
-      <Field type="text" name="email"/>
-      {props.errors.email && props.touched.email && (<p style={errorStyle}>{props.errors.email}</p>)}
+        <label htmlFor="email">Email:</label>
+        <Field type="text" name="email"/>
+        {props.errors.email && props.touched.email && (<p style={errorStyle}>{props.errors.email}</p>)}
 
-      <label htmlFor="password">Password:</label>
-      <Field type="password" name="password"/>
-      {props.errors.password && props.touched.password && (<p style={errorStyle}>{props.errors.password}</p>)}
+        <label htmlFor="password">Password:</label>
+        <Field type="password" name="password"/>
+        {props.errors.password && props.touched.password && (<p style={errorStyle}>{props.errors.password}</p>)}
 
-      <label htmlFor="termsOfService">Terms of Services:</label>
-      <Field type="checkbox" name="termsOfService" checked={values.termsOfService}/>
-      {props.errors.termsOfService && props.touched.termsOfService && (<p style={errorStyle}>{props.errors.termsOfService}</p>)}
+        <label htmlFor="termsOfService">Terms of Services:</label>
+        <Field type="checkbox" name="termsOfService" checked={props.values.termsOfService}/>
+        {props.errors.termsOfService && props.touched.termsOfService && (<p style={errorStyle}>{props.errors.termsOfService}</p>)}
 
-      <button type="submit">Submit</button>
-    </Form>
+        <button type="submit">Submit</button>
+      </Form>
+      <pre>{JSON.stringify(props.values, null, 2)}</pre>
+      <pre>{JSON.stringify(props.errors, null, 2)}</pre>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <div>{user.name}</div>
+            <div>{user.email}</div>
+            <div>{user.password}</div>
+          </div>
+        )
+      })}
+    </div>
   );
 }
 
@@ -66,13 +88,15 @@ const FormikApp = withFormik({
   }),
   handleSubmit(values, formikBag){
     console.log("submitting!", values);
+    console.log(formikBag)
     axios
-      .post("https://reqres.in/api/users/", values)
+      .post("https://reqres.in/api/users", values)
       .then(res => {
         console.log('success', res)
+        formikBag.setStatus(res.data)
+        formikBag.resetForm();
       })
       .catch(err => console.log(err))
-    // resetForm()
   }
 })(App)
 
